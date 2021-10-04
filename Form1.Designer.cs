ec16db919762fc247e326c44ce892959b62b6f8e
simoncmolina <52867420+simoncmolina@users.noreply.github.com>
2021-10-03 20:48:26 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace ProyectoFiveElements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textCliente = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.textDireccion = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Location = new System.Drawing.Point(12, 366);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersWidth = 62;
+            this.grid.RowTemplate.Height = 28;
+            this.grid.Size = new System.Drawing.Size(1232, 288);
+            this.grid.TabIndex = 0;
+            this.grid.Click += new System.EventHandler(this.grid_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(474, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FIVE ELEMENTS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IdCliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(365, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 31);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(682, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 31);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Teléfono";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(971, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Dirección";
+            // 
+            // textCliente
+            // 
+            this.textCliente.Location = new System.Drawing.Point(187, 129);
+            this.textCliente.Name = "textCliente";
+            this.textCliente.Size = new System.Drawing.Size(172, 26);
+            this.textCliente.TabIndex = 6;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(491, 130);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(172, 26);
+            this.textNombre.TabIndex = 7;
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Location = new System.Drawing.Point(803, 130);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(172, 26);
+            this.textTelefono.TabIndex = 8;
+            // 
+            // textDireccion
+            // 
+            this.textDireccion.Location = new System.Drawing.Point(1100, 130);
+            this.textDireccion.Name = "textDireccion";
+            this.textDireccion.Size = new System.Drawing.Size(125, 26);
+            this.textDireccion.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(371, 254);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(156, 50);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(624, 254);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(173, 50);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(881, 254);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(156, 50);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1256, 666);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.textDireccion);
+            this.Controls.Add(this.textTelefono);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textCliente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textCliente;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.TextBox textDireccion;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFiveElements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+        private void cargar()
+        {
+            BaseParqueaderoEntities contexto = new BaseParqueaderoEntities();
+            grid.DataSource = contexto.ClientesParqueadero.ToList();
+        }
+        private void llenar()
+        {
+            this.textCliente.Text = grid.SelectedRows[0].Cells[0].Value.ToString();
+            this.textNombre.Text = grid.SelectedRows[0].Cells[1].Value.ToString();
+            this.textTelefono.Text = grid.SelectedRows[0].Cells[2].Value.ToString();
+            this.textDireccion.Text = grid.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(this.textCliente.Text);
+            string Nombre = textNombre.Text;
+            string telefono = textTelefono.Text;
+            string Direccion = textDireccion.Text;
+
+            using (BaseParqueaderoEntities contexto = new BaseParqueaderoEntities())
+            {
+                ClientesParqueadero c = new ClientesParqueadero
+                {
+                    idCliente = id,
+                    Nombre = Nombre,
+                    Teléfono = telefono,
+                    Dirección = Direccion
+                };
+                contexto.ClientesParqueadero.Add(c);
+                contexto.SaveChanges();
+                cargar();
+            }
+        }
+
+        private void grid_Click(object sender, EventArgs e)
+        {
+            llenar();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt16(this.textCliente.Text);
+            string Nombre = textNombre.Text;
+            string telefono = textTelefono.Text;
+            string direccion = textDireccion.Text;
+
+            using (BaseParqueaderoEntities contexto = new BaseParqueaderoEntities())
+            {
+                ClientesParqueadero c = contexto.ClientesParqueadero.FirstOrDefault(x => x.idCliente == id);
+                c.Nombre = Nombre;
+                c.Teléfono = telefono;
+                c.Dirección = direccion;
+                contexto.SaveChanges();
+                cargar();
+                
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt16(this.textCliente.Text);
+            using (BaseParqueaderoEntities contexto = new BaseParqueaderoEntities())
+            {
+                ClientesParqueadero c = contexto.ClientesParqueadero.FirstOrDefault(x => x.idCliente == id);
+                contexto.ClientesParqueadero.Remove(c);
+                contexto.SaveChanges();
+                cargar();
+
+            }
+        }
+    }
+}
